c7944eae88ad052b563c99cfa46de0b5e03901a7
rosenlax <rosenlax@arcada.fi>
2025-02-03 20:03:38 +0200

    uppadeted the index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include "includes/functions.php";
+include "includes/counter.php"; 
 
 $user_ip = $_SERVER['REMOTE_ADDR'];
 $username = $_SESSION['username'] ?? 'Guest';
@@ -19,14 +20,13 @@ $first_visit = getFirstVisitTime();
 </head>
 
 <body>
-
     <div class="container">
-        <?php include "includes/header.php"; ?>
+        <?php include "header.php"; ?>
 
         <h2>Welcome to Match N' Meet</h2>
         <p>Find your perfect match today!</p>
 
-        <button class="toggle-butn" >Show Info</button>
+        <button class="toggle-butn">Show Info</button>
         <div class="info-box" id="infoBox">
             <p>Your IP Address: <?php echo $user_ip; ?></p>
             <p>Username: <?php echo $username; ?></p>
@@ -34,9 +34,14 @@ $first_visit = getFirstVisitTime();
             <p>Timezone: <?php echo $server_info['timezone']; ?></p>
             <p>First Visit: <?php echo $first_visit; ?></p>
         </div>
-        <script src="scripts/toggleInfo.js"></script>
 
-        <?php include "includes/footer.php"; ?>
+        <?php include "includes/login.php"; ?>  
+        <?php include "includes/countdown.php"; ?>  
+        <h3>Visitor Counter</h3>
+        <p>Unique Visitors: <strong><?php echo countVisitors(); ?></strong></p>
+
+        <script src="scripts/toggleInfo.js"></script>
+        <?php include "footer.php"; ?>
     </div>
 </body>
 </html>
